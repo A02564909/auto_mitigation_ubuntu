72970fc88cea4205a1a022e314cc0f98f639aa43
A02564909 <A02564909@alabama.edu>
2024-10-18 14:38:29 -0500

    Update ubuntu_notes.sh

--- ubuntu_notes.sh
+++ ubuntu_notes.sh
@@ -6,9 +6,11 @@ apt-get install nettools
 echo "The output of the netstat command that displays all the ports." >> ubuntu_notes.txt
 netstat -a >> ubuntu_notes.txt
 #How to close a port
+echo"Make sure to use ufw deny command to deny unwanted open ports."
 #Check for shared files and folders
 #Audit Policies
 #Check for null passwords
 #Disable Root Login
 #nginx Services
 #FTP Service
+#Change Password Expiration Period
