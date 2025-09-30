249ace93b56e6530d56c6d13b8342ebbbb075891
A02564909 <A02564909@alabama.edu>
2025-09-30 13:48:41 -0500

    Update ubuntu_listsnnotes.sh

--- ubuntu_listsnnotes.sh
+++ ubuntu_listsnnotes.sh
@@ -1,16 +1,23 @@
 #!usr/bin/env bash
-#PW Policy
-#Lockout Policy
+
 #Open and Listening Ports
 apt-get install nettools
 echo "The output of the netstat command that displays all the ports." >> ubuntu_notes.txt
 netstat -a >> ubuntu_notes.txt
+
+echo "The output of the ss command that displays all the ports and processes of listening ports:" >> ubuntu_notes.txt
+echo "(You DEFINITELY DO NOT want FTP (vsftpd) or nginx in the system)" >> ubuntu_notes.txt
+ss -tlnp >> ubuntu_notes.txt
+
+#List Services
+echo "The output of the systemctl command (list-unit-files) that displays all of the unit files:" >> ubuntu_notes.txt
+systemctl list-unit-files >> ubuntu_notes.txt
+
+echo "The ouput of the systemctl command (list-unit-files --state=masked) that displays masked unit files:" >> ubuntu_notes.txt
+
 #How to close a port
 echo"Make sure to use ufw deny command to deny unwanted open ports."
+
 #Check for shared files and folders
-#Audit Policies
-#Check for null passwords
-#Disable Root Login
-#nginx Services
-#FTP Service
-#Change Password Expiration Period
+echo "The output of the ls -alF command that displays read, write, and execute permission for the /etc directory." >> ubuntu_notes.txt
+ls -alF /etc >> ubuntu_notes.txt
