9e541a659730d0ce3fc441f7dc8fc4770ad2ac84
A02564909 <A02564909@alabama.edu>
2025-11-13 18:49:55 -0600

    Update linxUpdates2.sh

--- scripts/linxUpdates2.sh
+++ scripts/linxUpdates2.sh
@@ -6,3 +6,5 @@ apt full-upgrade -y # Upgrades packages and Installs new dependencies
 apt autoremove -y  # Remove unnecessary packages
 apt clean -y       # Removes unnecessary package files to clean local repositories.
 #apt install -f -y  # Fixes broken dependencies--use only if indicated by errors.
+
+read -p "Updates were run--restart may be necessary. Press enter to continue."
