18bd657a1f6d9adc638d70cb933609a8b3124708
A02564909 <A02564909@alabama.edu>
2025-11-13 18:17:47 -0600

    Update passwordExpiration.sh

--- scripts/security/passwordExpiration.sh
+++ scripts/security/passwordExpiration.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 clear 
 
-sed -i.bk 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS 5/' /etc/login.defs
+sed -i.bk 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS 10/' /etc/login.defs
 sed -i 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS 60/' /etc/login.defs
-sed -i 's/^PASS_WARN_AGE.*/PASS_WARN_AGE 60/' /etc/login.defs
+sed -i 's/^PASS_WARN_AGE.*/PASS_WARN_AGE 5/' /etc/login.defs
 
 read -p "Password Expiration configured. Press Enter to continue...."
