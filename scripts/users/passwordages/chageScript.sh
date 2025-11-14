d46df56bd9ea16608dfa24720a93ca3739cba970
A02564909 <A02564909@alabama.edu>
2025-11-13 20:24:42 -0600

    Update chageScript.sh

--- scripts/users/passwordages/chageScript.sh
+++ scripts/users/passwordages/chageScript.sh
@@ -5,7 +5,7 @@ clear
 mapfile -t users < needfix.txt
 
 for user in "${users[@]}"; do
-	if id "$user" &>/dev/null; then # Check if user id is valid
+	if id "$user" &>/dev/null; then # Check if user id is valid--"&>dev/null" prevents every single user's id from printing on output.
 			# Fix password expiration periods
         	sudo chage -M 90 -m 10 -W 5 "$user"
         	echo "Updated password max days for $user"
