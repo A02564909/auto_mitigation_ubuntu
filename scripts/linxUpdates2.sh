48e38c52f2217b6a0c86a076a54fd5f5c58e00b3
A02564909 <A02564909@alabama.edu>
2025-11-13 18:49:05 -0600

    Update and rename Ubuntu24_updates2_script.sh to linxUpdates2.sh

---
+++ scripts/linxUpdates2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+clear
+
+apt update
+apt full-upgrade -y # Upgrades packages and Installs new dependencies
+apt autoremove -y  # Remove unnecessary packages
+apt clean -y       # Removes unnecessary package files to clean local repositories.
+#apt install -f -y  # Fixes broken dependencies--use only if indicated by errors.
